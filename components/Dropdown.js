775013b19d07a7d4bdb1fec23073831039fc9033
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-01-11 15:48:58 -0500

    Fixed dropdown color bug

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -3,7 +3,7 @@ import Collapsible from "react-collapsible"
 const Dropdown = ({triggerWord, children}) => {
     return (
         <div className='border-2 border-black'>
-            <Collapsible className="bg-white" trigger={triggerWord} transitionTime={100}>
+            <Collapsible className="bg-white dark:bg-[var(--cat-mocha-crust)]" trigger={triggerWord} transitionTime={100}>
                 <div className='dropdown-content'>
                     {children}
                 </div>
