9dc74013b092b922e37cc2341d4f53920c2e9948
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-01-12 07:37:48 -0500

    Rounded corners

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -2,7 +2,7 @@ import Collapsible from "react-collapsible"
 
 const Dropdown = ({triggerWord, children}) => {
     return (
-        <div className='border-2 border-black'>
+        <div className='border-2 border-black rounded-[10px] overflow-hidden'>
             <Collapsible className="bg-white dark:bg-[var(--cat-mocha-crust)]" trigger={triggerWord} transitionTime={100}>
                 <div className='dropdown-content'>
                     {children}
