3227ac2cebb461484e0c906c9b311cd25c4eabf3
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-01-28 09:56:55 -0500

    Changed header for mobile

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Rocket from "../public/rocket-book.png"
 const Header = ({ children }) => {
     return (
         <div>
-            <header className='bg-[var(--cat-mocha-base)] border-solid border-2 border-black dark:border-[var(--cat-mocha-text)]'>
+            <header className='sm:bg-[var(--cat-mocha-base)] border-solid sm:border-2 border-black dark:border-[var(--cat-mocha-text)]'>
                 <br /><br /><br /><br />
                 <Image
                     src={Rocket}
