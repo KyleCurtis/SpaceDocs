7a7472a22d45a86c5e17e1fefbfafd888def32f4
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-02-13 16:15:31 -0500

    Updated fonts

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -1,14 +1,8 @@
 import {
-  Martian_Mono,
   Exo_2,
   Source_Code_Pro,
 } from "@next/font/google";
 
-export const martian_mono = Martian_Mono({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--martian-mono",
-});
 
 export const exo2 = Exo_2({
   display: "swap",
